afd420bf2e5ed0b0b73972d0884ea48e77c014b5
hyunjung <asroq98@gmail.com>
2025-09-29 22:55:55 +0900

    update : CRUD Posting feature

--- src/main/java/me/hyunjung/SpringbootDeveloperApplication.java
+++ src/main/java/me/hyunjung/SpringbootDeveloperApplication.java
@@ -2,7 +2,9 @@ package me.hyunjung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 
 @SpringBootApplication
 public class SpringbootDeveloperApplication {
 
